3fba9b1238da6a4b731436eef1493db5e5c31772
Lukas Woodtli <woodtli.lukas@gmail.com>
2014-12-25 00:08:15 +0100

    Public include directory has now right name

---
+++ include/FeaturesTogglePlusPlus/Feature.h
@@ -0,0 +1,58 @@
+
+#ifndef __FEATURE_H__
+#define __FEATURE_H__
+
+namespace FeaturesTogglePlusPlus
+{
+
+
+
+#define DISABLE_COPY_AND_ASSIGN(TypeName) \
+TypeName(const TypeName&);   \
+void operator=(const TypeName&)
+
+
+
+class StaticFeature
+{
+    DISABLE_COPY_AND_ASSIGN(StaticFeature);
+public:
+    StaticFeature() : m_enabled(false) {}
+    StaticFeature(bool enable) : m_enabled(enable) {}
+
+    virtual ~StaticFeature(void) {}
+    virtual bool isEnabled() const
+    {
+        return m_enabled;
+    }
+
+private:
+    const bool m_enabled;
+
+};
+
+class DynamicFeature
+{
+    DISABLE_COPY_AND_ASSIGN(DynamicFeature);
+public:
+    DynamicFeature(bool enable) : m_enabled(enable) {}
+
+    ~DynamicFeature(void) {}
+    void enable(bool enable)
+    {
+        m_enabled = enable;
+    }
+
+    bool isEnabled() const
+    {
+        return m_enabled;
+    }
+
+private:
+    bool m_enabled;
+
+};
+
+}
+
+#endif /* __FEATURE_H__ */
---
+++ include/FeaturesTogglePlusPlus/FeatureToggles.h
@@ -0,0 +1,83 @@
+/************************************************************************************/
+/*! \file
+ 
+ - Project           : FeatureToggles++
+ - Functional Block  : Feature List
+ 
+ License: This project has not a license yet. You can do what ever you want with
+ this code. Just don't blame me if something goes wrong!
+ 
+ This is the main entry point to get access to the feature objects and the information
+ they store. Use the macro getFeature(id) to get a reference to a feature object.
+ 
+ */
+/************************************************************************************/
+
+#ifndef __FEATURETOGGLE_H__
+#define __FEATURETOGGLE_H__
+
+#include "Feature.h"
+
+namespace FeaturesTogglePlusPlus
+{
+
+
+enum featureId_t
+{
+#define FEATURE_DEFINE_MACRO FEATURE_DEFINE_MACRO_ID
+#include "FeatureDefinesMacros.xdef"
+    FEATURE_NMB
+};
+
+
+template<featureId_t id>
+struct idToType_t
+{};
+
+
+
+
+
+
+#define FEATURE_DEFINE_MACRO FEATURE_DEFINE_MACRO_MAP_FEATURE_TO_TYPE
+#include "FeatureDefinesMacros.xdef"
+
+
+
+
+
+void * const __getFeature(featureId_t id);
+
+#define getFeature(id) _getFeature<id>()
+
+
+
+template <featureId_t id>
+typename idToType_t<id>::Type& _getFeature()
+{
+    typedef typename idToType_t<id>::Type T;
+    T * const pFeature = static_cast<T*>(__getFeature(id));
+    T & feature = *pFeature;
+    return feature;
+}
+
+
+#define featureEnabled(id) _featureEnabled<id>()
+
+template <featureId_t Id>
+bool _featureEnabled()
+{
+    typedef typename idToType_t<Id>::Type T;
+    const T & feature = _getFeature<Id>();
+    return feature.isEnabled();
+}
+
+
+
+// ifdef
+char const * const _getFeatureName(featureId_t id);
+
+
+}
+
+#endif /* __FEATURETOGGLE_H__ */
